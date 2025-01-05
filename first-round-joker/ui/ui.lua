f244c3d842db9429f3ec8d3c6026c1601568a2be
LnxFCA <dev@lnxfca.com>
2025-01-05 12:09:23 -0500

    first-round-joker: Refactor UI files
    (cherry picked from commit c7ca6ffbb2f64004dcc1baee77dc803cc967226a)

--- first-round-joker/ui/ui.lua
+++ first-round-joker/ui/ui.lua
@@ -1,5 +1,7 @@
-include("ui/config_tab.lua")
-
 FRJ_UI = {
-    config_tab = config_tab
+    config_tab = nil,
+    card_selection_ui = nil
 }
+
+include("ui/config_tab.lua")
+include("ui/card_selection.lua")
