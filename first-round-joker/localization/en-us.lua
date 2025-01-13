2ecbef65da54c0478be289dbc5b700cf6e665103
LnxFCA <dev@lnxfca.com>
2025-01-12 19:08:59 -0500

    first-round-joker: Add custom keybind support
    This commit adds a new mod option that allows the user to
    set a custom keybind to activate the mod.
    
    (cherry picked from commit e8d56047f607b0531884e4cac86cceb39fdb6103)

--- first-round-joker/localization/en-us.lua
+++ first-round-joker/localization/en-us.lua
@@ -4,7 +4,9 @@ return {
             frj_save_joker = "Save selected Joker",
             frj_save_joker_d = "Keeps the Joker selected even after closing the game.",
             frj_base_price = "Base price",
-            frj_base_price_d = "Use the Joker's base price (price without edition effect)"
+            frj_base_price_d = "Use the Joker's base price (price without edition effect)",
+            frj_user_keybind = "Use custom keybind",
+            frj_user_keybind_d = "Custom keyboard shortcut to activate the mod. Default: F",
         }
     }
 }
