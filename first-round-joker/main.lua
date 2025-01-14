d299aea8604a97187103f69845b7b5d77482cc8c
LnxFCA <dev@lnxfca.com>
2025-01-14 04:07:11 -0500

    first-round-joker: v2 refactor
    This commits improves the mod code structure and improves
    readability.
    
    Changes:
    - Move most of the mod code to `frjm`
    - All game overrides are now in `frjm/overrides.lua`
    - Improve mod initialization
    - Improve general code organization
    
    (cherry picked from commit 9211f4c609fd0f9608f79cc9355ea560dd0c0368)

--- first-round-joker/main.lua
+++ first-round-joker/main.lua
@@ -1,88 +1,52 @@
 -- mod globals
 FRJM = {}
-FRJM.UI = {}
-FRJM.original = {}
-FRJM.utils = {}
-FRJM.config = {}  -- runtime
 
-FRJM.mod = SMODS.current_mod
-FRJM.mod_id = FRJM.mod.id or "first-round-joker"
+FRJM.init = function (self)
+    -- initialize globals
+    self.UI = {}
+    self.original = {}
+    self.utils = {}
+    self.config = {}  -- runtime
 
+    self.mod = SMODS.current_mod
+    self.mod_id = FRJM.mod.id or "first-round-joker"
 
--- Save mod configuration
-SMODS.save_mod_config(FRJM.mod)
+    -- initialize runtime configuration
+    local mconfig = self.mod.config
 
+    self.config.enable = true
+    self.config.joker_key = (mconfig.save_joker and mconfig.joker_key) or nil
+    self.config.keybind = mconfig.default_keybind
 
---- Load mod runtime configuration
-FRJM.utils.load_config = function ()
-    local config = {}
-
-    config.enable = true
-    config.joker_key = (FRJM.mod.config.save_joker and FRJM.mod.config.joker_key) or nil
-    config.keybind = FRJM.mod.config.default_keybind
-
-    if FRJM.mod.config.use_user_keybind and FRJM.mod.config.user_keybind ~= "" then
-        config.keybind = FRJM.mod.config.user_keybind
+    if mconfig.use_user_keybind and mconfig.user_keybind ~= "" then
+        self.config.keybind = mconfig.user_keybind
     end
 
-    config.keybind = string.lower(config.keybind)
-
-    FRJM.config = config
-end
+    self.config.keybind = string.lower(self.config.keybind)
 
 
----@param filename string
-FRJM.utils.include = function (filename)
-    SMODS.load_file(filename, FRJM.mod_id)()
-end
-
-
--- Whatever the mod should perform or not
----@return boolean
-FRJM.utils.enabled = function (area)
-    local enabled = FRJM.config.enable -- true at the
-    local enabled_for_round = false -- true on round == 1
-    local enabled_for_game = false -- false if tutorial in progress
-    local enabled_for_key = false -- true if FRJM.config.joker_key isn't nil
-    local enabled_for_area = false -- true if area == G.shop_jokers
-
-    if enabled then
-        enabled_for_round = G.GAME.round == 1
-        enabled_for_game = not (G.SETTINGS.tutorial_progress and G.SETTINGS.tutorial_progress.forced_shop)
-        enabled_for_key = FRJM.config.joker_key ~= nil
-        enabled_for_area = G.shop_jokers and area == G.shop_jokers
-
-        enabled = enabled_for_round
-            and enabled_for_game
-            and enabled_for_key
-            and enabled_for_area
+    -- include func
+    ---@param filename string
+    self.include = function (filename)
+        SMODS.load_file(filename, self.mod_id)()
     end
 
-    return enabled
+
+    -- self (parent)
+    self.utils.parent = function () return self end
+
+    -- save init configuration
+    SMODS.save_mod_config(self.mod)
 end
 
--- Mod starup
-FRJM.utils.include("frjm/ui/config_tab.lua")
-FRJM.utils.include("frjm/overrides.lua")
 
-FRJM.utils.load_config()
+-- FRJM starup
+FRJM:init()
 
-
--- save configuration
-FRJM.utils.save_config = function()
-    -- save the current Joker key if save_joker is enabled
-    if FRJM.mod.config.save_joker then
-        FRJM.mod.config.joker_key = FRJM.config.joker_key
-    else
-        FRJM.mod.config.joker_key = nil
-    end
-
-        if FRJM.mod.config.user_keybind == "" or (not FRJM.mod.config.user_keybind) then
-            FRJM.mod.config.user_keybind  = FRJM.mod.config.default_keybind
-        end
-
-    SMODS.save_mod_config(FRJM.mod)
-end
+FRJM.include("frjm/overrides.lua")
+FRJM.include("frjm/utils.lua")
+FRJM.include("frjm/ui/config_tab.lua")
+FRJM.include("frjm/ui/card_selection.lua")
 
 
 FRJM.utils.keyevent = function (self)
@@ -93,7 +57,7 @@ FRJM.utils.keyevent = function (self)
     then -- get the card key and save it if `save_joker` is enabled
         FRJM.config.joker_key = card.config.center.key
         if FRJM.mod.config.save_joker then
-            FRJM.utils.save_config()
+            FRJM:save_config()
         end
     end
 end
