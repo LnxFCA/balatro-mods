ff43e51aa075697a0a359900ad7d78a53f74f574
LnxFCA <dev@lnxfca.com>
2025-01-14 07:44:53 -0500

    first-round-joker: Add documentation/comments to source code
    (cherry picked from commit ad3c813184891988dae35a1d81f9db1443a7a536)

--- first-round-joker/frjm/ui/card_selection.lua
+++ first-round-joker/frjm/ui/card_selection.lua
@@ -33,16 +33,20 @@ FRJM.UI.create_card_selection_ui = function ()
     local jokers_collection = {}
     local jokers = {}
 
+    -- filter jokers
     for _, v in ipairs(G.P_CENTER_POOLS.Joker) do
         if v.discovered then table.insert(jokers, v) end
     end
 
+    -- generate collection UI
     jokers_collection = SMODS.card_collection_UIBox(jokers, { 4, 4, 4}, {
         no_materialize = true,
         h_mod = 0.95,
         back_func = 'exit_overlay_menu'
     })
 
+    -- insert selection information UI
+    ---@see create_UIBox_generic_options
     table.insert(jokers_collection.nodes[1].nodes[1].nodes, 2, FRJM.UI.selected_card_ui())
 
     ---@type UIDef
