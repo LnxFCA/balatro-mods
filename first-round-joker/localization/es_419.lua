b88d18465134a1e34e7e689cf95698d3e57f6201
LnxFCA <dev@lnxfca.com>
2025-01-15 19:21:09 -0500

    first-round-joker: Fix broken non-English localization
    (cherry picked from commit a315af85f59b266666a55dc6c1b8827ad978e76a)

--- first-round-joker/localization/es_419.lua
+++ first-round-joker/localization/es_419.lua
@@ -2,10 +2,16 @@ return {
     misc = {
         dictionary = {
             frj_save_joker = "Guardar Joker seleccionado",
-            frj_save_joker_d = "Guarda la carta Joker seleccionada entre partidas, asegurando que esté disponible al reiniciar el juego.",
+            frj_save_joker_d = {
+                "Guarda la carta Joker seleccionada entre partidas",
+                "asegurando que esté disponible al reiniciar el juego.",
+            },
 
             frj_base_price = "Usar precio base del Joker",
-            frj_base_price_d = "Configura el precio de la carta Joker a su valor original, sin tomar en cuenta los costos adicionales de los efectos o ediciones aplicadas.",
+            frj_base_price_d = {
+                "Configura el precio de la carta Joker a su valor original",
+                "sin tomar en cuenta los costos adicionales de los efectos o ediciones aplicadas.",
+            },
 
             frj_use_custom_keybind = "Habilitar tecla personalizada",
             frj_use_custom_keybind_d = {
@@ -15,6 +21,6 @@ return {
 
             frj_joker_text_selected = "FRJ: ",
             frj_game_restart_btn = "Reiniciar juego",
-        }
-    }
+        },
+    },
 }
