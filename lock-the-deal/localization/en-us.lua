e2fedc6482f07cd2aee487b5317b2165c9bc0e8a
LnxFCA <dev@lnxfca.com>
2025-01-25 14:27:35 -0500

    LTD: Improve US localization
    (cherry picked from commit de4610affcd64ca4f28253ec762480cd8f7600ad)

--- lock-the-deal/localization/en-us.lua
+++ lock-the-deal/localization/en-us.lua
@@ -3,28 +3,28 @@ return {
         dictionary = {
             ltd_frjm_integration = "Enable FRJM Integration",
             ltd_frjm_integration_d = {
-                "If enabled, automatically locks the FRJM selected Joker"
+                "If enabled, automatically locks the Joker card selected by FRJM.",
             },
 
             ltd_lock_keybind = "Enable Lock/Unlock Keybind",
             ltd_lock_keybind_d = {
-                "If enabled, when the specified keybind is pressed and a valid card",
-                "is being hovered, it will works as the card's Lock/Unlock button"
+                "If enabled, pressing the specified keybind while hovering over",
+                "a valid card will act as the card's Lock/Unlock button.",
             },
 
-            ltd_lock_keybind_invalid = "Invalid key",
-            ltd_lock_keybind_exist = "Key already in use",
+            ltd_lock_keybind_invalid = "Invalid key.",
+            ltd_lock_keybind_exist = "Key already in use.",
 
             ltd_button_lock = "LOCK",
             ltd_button_locked = "LOCKED",
 
-            ltd_button_tooltip_title = "Cost",
+            ltd_button_tooltip_title = "Lock Cost",
             ltd_button_tooltip_free = "Free",
 
-            ltd_booster_pack_enable = "Enable Booster Pack Lock",
+            ltd_booster_pack_enable = "Enable Booster Pack Locking",
             ltd_booster_pack_enable_d = {
-                "Allow locking booster packs",
+                "Allow locking booster packs in the shop.",
             },
-        }
-    }
+        },
+    },
 }
