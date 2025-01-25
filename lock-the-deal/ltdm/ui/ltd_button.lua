41a6673196733471c40c9142cc8cc5fe04d570b4
LnxFCA <dev@lnxfca.com>
2025-01-25 14:31:33 -0500

    LTD: Improve localization support

--- lock-the-deal/ltdm/ui/ltd_button.lua
+++ lock-the-deal/ltdm/ui/ltd_button.lua
@@ -1,15 +1,127 @@
----@param card LTDM.Card
-function LTDM.UIDEF.add_ltd_button(card)
-    local offset = { x = -0.55, y = 0 }
+local ltd_button_defaults = {
+    {
+        width = 2.2,
+        label_scale = 0.4,
+        locked_offset_x = {
+            voucher = -0.66,
+            other = -0.46,
+            consumeable = -0.52,
+            booster = -0.75,
+        },
+        lock_offset_x = {
+            voucher = -0.85,
+            other = -0.64,
+            consumeable = -0.72,
+            booster = -0.9,
+        },
+        languages = { 'es_ES', 'es_419', 'pt_BR', }
+    },
+    {
+        width = 1.8,
+        label_scale = 0.4,
+        locked_offset_x = {
+            voucher = -0.42,
+            other = -0.23,
+            consumeable = -0.3,
+            booster = -0.48,
+        },
+        lock_offset_x = {
+            voucher = -0.8,
+            other = -0.6,
+            consumeable = -0.7,
+            booster = -0.86,
+        },
+        languages = { 'it' }
+    },
+    {
+        width = 1.8,
+        label_scale = 0.4,
+        locked_offset_x = {
+            voucher = -0.45,
+            other = -0.22,
+            consumeable = -0.32,
+            booster = -0.5,
+        },
+        lock_offset_x = {
+            voucher = -0.65,
+            other = -0.42,
+            consumeable = -0.52,
+            booster = -0.68,
+        },
+        languages = { 'de' }
+    },
+    {
+        width = 2.4,
+        label_scale = 0.4,
+        locked_offset_x = {
+            voucher = -0.66,
+            other = -0.46,
+            consumeable = -0.56,
+            booster = -0.75,
+        },
+        lock_offset_x = {
+            voucher = -0.55,
+            other = -0.35,
+            consumeable = -0.4,
+            booster = -0.55,
+        },
+        languages = { 'fr', }
+    },
+    default = {
+        width = 1.6,
+        label_scale = 0.5,
+        locked_offset_x = {
+            voucher = -0.35,
+            other = -0.15,
+            consumeable = -0.25,
+            booster = -0.43,
+        },
+        lock_offset_x = {
+            voucher = -0.75,
+            other = -0.54,
+            consumeable = -0.65,
+            booster = -0.8,
+        },
+    },
+}
 
-    -- Fix alignment
-    if card.ability.consumeable or card.ability.set == 'Booster' then
-        offset.x = -0.65
+-- Process list
+local ltd_button_conf = {}
+for _, v in ipairs(ltd_button_defaults) do
+    for _, l in ipairs(v.languages) do
+        ltd_button_conf[l] = v
+    end
+    v.languages = nil
+end
+
+
+--- Get the default configartion for current language
+---@return LTDM.ButtonConfig
+function LTDM.UIDEF.get_ltd_button_conf()
+    local config = ltd_button_conf[G.SETTINGS.language]
+    if not config then return ltd_button_defaults.default end
+
+    -- Load defaults
+    for k, v in pairs(ltd_button_defaults.default) do
+        if not config[k] then config[k] = v end
     end
 
-    -- Fix Voucher alignment
-    if card.ability.set == 'Voucher' then
-        offset.x = -0.75
+    return config
+end
+
+
+---@param card LTDM.Card
+function LTDM.UIDEF.add_ltd_button(card)
+    local ltd_btn_config = LTDM.UIDEF.get_ltd_button_conf()
+    local offset = { x = ltd_btn_config.lock_offset_x.other, y = 0 }
+
+    -- Fix alignment
+    if card.ability.set == 'Booster' then
+        offset.x = ltd_btn_config.lock_offset_x.booster
+    elseif card.ability.consumeable then
+        offset.x = ltd_btn_config.lock_offset_x.consumeable
+    elseif card.ability.set == 'Voucher' then
+        offset.x = ltd_btn_config.lock_offset_x.voucher
         offset.y = 0.2
     end
 
@@ -18,15 +130,15 @@ function LTDM.UIDEF.add_ltd_button(card)
         n = G.UIT.ROOT,
         config = {
             id = 'ltd_button', ref_table = card, minh = 0.8, padding = 0.1, align = "cr",
-            shadow = true, r = 0.08, minw = 1.6, button = 'ltd_lock_unlock',
-            hover = true, func = 'ltd_can_lock_unlock',
+            shadow = true, r = 0.08, minw = ltd_btn_config.width, button = 'ltd_lock_unlock',
+            hover = true, func = 'ltd_can_lock_unlock', emboss = 0.05, ltd_btn_conf = ltd_btn_config,
         },
         nodes = {{
             n = G.UIT.O,
             config = {
                 object = DynaText({
                     string = {{ ref_table = card.ltdm_state.button, ref_value = 'label' }},
-                    colours = { G.C.UI.TEXT_LIGHT }, scale = 0.5, x_offset = offset.x,
+                    colours = { G.C.UI.TEXT_LIGHT }, scale = ltd_btn_config.label_scale or 0.5, x_offset = offset.x,
                 })
             },
         }},
