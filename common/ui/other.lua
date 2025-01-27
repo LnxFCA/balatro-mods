b1a4bdb095e84b8392de196d7244c8b22341cd7e
LnxFCA <dev@lnxfca.com>
2025-01-26 20:30:25 -0500

    common: Add comments
    (cherry picked from commit 0269c627219a14ff64602545518e97a6fc99f288)

--- common/ui/other.lua
+++ common/ui/other.lua
@@ -9,8 +9,11 @@ end
 
 function LNXFCA.UIDEF.create_open_button(args)
     if not G.FUNCS.lnxfca_open_button then
+        -- Define the button handling function
         G.FUNCS.lnxfca_open_button = function (e)
             if e.config.callback then e.config.callback(e) end
+
+            -- ref_value holds the link to open
             LNXFCA.utils.open_link(e.config.ref_value)
         end
     end
@@ -29,10 +32,11 @@ end
 
 
 function LNXFCA.UIDEF.create_open_button_grid(rows, args, spacing)
-    local buttons = {}
+    local buttons = {} -- UIDef[]
     local parent = { n = G.UIT.R, config = { align = "cm", padding = 0.1, }, nodes  = {}}
     for i, link in ipairs(args) do
-        if i % (rows + 1) == 0 then
+        if i % (rows + 1) == 0 then  -- Check for rows
+            -- Store parent and create a new container
             table.insert(buttons, parent)
             parent = { n = G.UIT.R, config = { align = "cm", padding = 0.1, }, nodes = {}}
         end
@@ -42,7 +46,10 @@ function LNXFCA.UIDEF.create_open_button_grid(rows, args, spacing)
             fg_colour = link.fg_colour,
         })
 
+        -- Insert button into the grid
         table.insert(parent.nodes, button)
+
+        -- Add button spacing
         if i % rows ~= 0 and #args > i and spacing then
             table.insert(parent.nodes, LNXFCA.UIDEF.create_spacing_box({ col = G.UIT.C, w = spacing }))
         end
