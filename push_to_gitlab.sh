f51e3fb34daa7882ea31322c1eafc4356fd1d9dc
LnxFCA <dev@lnxfca.com>
2025-03-20 03:04:38 -0500

    Update push_to_gitlab.sh

--- push_to_gitlab.sh
+++ push_to_gitlab.sh
@@ -7,8 +7,10 @@ then
   git subtree push --prefix=common shared-gitlab main:main
   git subtree push --prefix=first-round-joker frjm-gitlab main:main
   git subtree push --prefix=lock-the-deal ltdm-gitlab main:main
+  git subtree push --prefix=hermit-level-up hlum-gitlab main:main
 else
   git subtree push --prefix=common shared-gitlab gitlab-release:release
   git subtree push --prefix=first-round-joker frjm-gitlab gitlab-release:release
   git subtree push --prefix=lock-the-deal ltdm-gitlab gitlab-release:release
+  git subtree push --prefix=hermit-level-up hlum-gitlab gitlab-release:release
 fi
