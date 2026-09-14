106788f7f903c345dab22ddf1a367b4c090d0a20
sonyxu <sonyxu@users.noreply.github.com>
2026-09-14 19:41:41 +0800

    Create zh_CN.lua for Chinese localization
    Add Chinese localization for game features and descriptions.

---
+++ lock-the-deal/localization/zh_CN.lua
@@ -0,0 +1,40 @@
+return {
+    misc = {
+        dictionary = {
+            ltd_frjm_integration = "启用 FRJM 集成",
+            ltd_frjm_integration_d = {
+                "如果启用，将自动锁定由 FRJM 选择的小丑牌。",
+            },
+
+            ltd_lock_keybind = "启用锁定/解锁按键绑定",
+            ltd_lock_keybind_d = {
+                "如果启用，在悬停于一张有效卡牌上时按下指定按键，",
+                "将等同于该卡牌的锁定/解锁按钮。",
+            },
+
+            ltd_lock_keybind_invalid = "无效按键。",
+            ltd_lock_keybind_exist = "按键已被占用。",
+
+            ltd_button_lock = "锁定",
+            ltd_button_locked = "已锁定",
+
+            ltd_controller_lock = "解锁",
+            ltd_controller_locked = "已解锁",
+
+            ltd_button_tooltip_title = "锁定费用",
+            ltd_button_tooltip_free = "免费",
+
+            ltd_booster_pack_enable = "启用补充包锁定",
+            ltd_booster_pack_enable_d = {
+                "允许在商店中锁定补充包。",
+            },
+
+            ltd_mod_about_label = "关于",
+            ltd_mod_description = {
+                "Lock the Deal 让你能够为后续回合或刷新锁定商店物品，",
+                "从而增强你的 Balatro 游戏体验，",
+                "为你的对局增添更多灵活性与策略性。",
+            },
+        },
+    },
+}
